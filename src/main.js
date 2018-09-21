6d28bf61cbbef902f89ec3be9b0ec5313a70f6bf
崔海峰 <cuihaifeng1919@hotmail.com>
2018-09-21 19:13:11 +0800

    初始化模板

---
+++ src/main.js
@@ -0,0 +1,5 @@
+
+let name = process.env.NODE_ENV
+export default {
+  name
+}
---
+++ test/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="test">小崔2</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'APP'
+}
+</script>
+
+<style lang="stylus" scoped>
+.test
+  color red
+  width 20px
+</style>
