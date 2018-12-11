47a88d74481245ae2d5cf27b6be4d4a95e69b0e3
崔海峰 <cuihaifeng@adeMacBook-Pro-2.local>
2018-12-11 14:23:53 +0800

    增加markdown解析

--- src/main.js
+++ src/main.js
@@ -1,19 +1,4 @@
-
-//? 崔海峰
-//? 测试用例
-export function test () {
-}
-//? 崔海峰
-//? 设置原生title
-export function result () {
-}
-//? 崔海峰
-//? 模块规范
-export function block () {
-}
-
-
-/* @init<%
+/* @init<%
 //? ${bridgeH5Man}
 //? ${bridgeEffect}
 export function ${bridgeName} () {
